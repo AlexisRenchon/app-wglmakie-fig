2b0677f64c7803d82b2a46684ff14ed3372aedcf
AlexisRenchon <a.renchon@gmail.com>
2023-01-24 14:51:19 -0500

    fix

--- src/JSServe_app.jl
+++ src/JSServe_app.jl
@@ -1,3 +1,6 @@
 using JSServe
-server = JSServe.Server(App(DOM.div("hello world")), "0.0.0.0", parse(Int,ARGS[1]))
+
+server = JSServe.Server(my_app, "0.0.0.0", parse(Int, ENV["PORT"]))
+route!(server, "/" => App(DOM.div("hello world")))
+
 wait(server)
