94ad8b904b7226c5200ff809c7b7f022354d0943
AlexisRenchon <a.renchon@gmail.com>
2023-01-24 14:43:12 -0500

    fix

--- src/JSServe_app.jl
+++ src/JSServe_app.jl
@@ -1,2 +1,3 @@
 using JSServe
-JSServe.Server(App(DOM.div("hello world")), "0.0.0.0", parse(Int,ARGS[1]), wait(server))
+server = JSServe.Server(App(DOM.div("hello world")), "0.0.0.0", parse(Int,ARGS[1]))
+wait(server)
--- src/JSServe_app.jl~
+++ src/JSServe_app.jl~
@@ -1,3 +1,2 @@
 using JSServe
-JSServe.Server(App(DOM.div("hello world")), "0.0.0.0", parse(Int,ARGS[1]))
-wait(server)
+JSServe.Server(App(DOM.div("hello world")), "0.0.0.0", parse(Int,ARGS[1]), wait(server))
