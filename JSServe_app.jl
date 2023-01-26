292326447f56dc21e28d185de033db2b51a07e3d
AlexisRenchon <a.renchon@gmail.com>
2023-01-26 09:20:12 -0500

    demo

--- JSServe_app.jl
+++ JSServe_app.jl
@@ -1,15 +1,34 @@
 using JSServe
-using parameterization_viz 
+using WGLMakie 
 
-my_app = param_dashboard(
-                         SoilCO2ModelParameters,
-                         Dict("CO2 production" => (d1, d2, p) -> microbe_source(d1, d2, 5.0, p), "CO2 diffusivity" => co2_diffusivity),
-                         ["T_soil", "M_soil"],
-                         ([273, 303], [0.0, 0.5])
-                        )
+function demo(
+        fig = Figure()
+        ax = Axis(fig[1, 1])
+        sg = SliderGrid(
+            fig[1, 2],
+            (label = "Voltage", range = 0:0.1:10, format = "{:.1f}V", startvalue = 5.3),
+            (label = "Current", range = 0:0.1:20, format = "{:.1f}A", startvalue = 10.2),
+            (label = "Resistance", range = 0:0.1:30, format = "{:.1f}Ω", startvalue = 15.9),
+            width = 350,
+            tellheight = false)
+        sliderobservables = [s.value for s in sg.sliders]
+        bars = lift(sliderobservables...) do slvalues...
+                [slvalues...]
+        end
+        barplot!(ax, bars, color = [:yellow, :orange, :red])
+        ylims!(ax, 0, 30)
+        fig
+        )
 
-# my_app = App(DOM.div("hello world"))
+fig = demo()
+
+my_app = App() do session::Session    
+    return DOM.div(fig) 
+end
+
+app_name = get(ENV, "HEROKU_APP_NAME", "jsserve-app-test")
+url = "https://$(app_name).herokuapp.com"
 server = JSServe.Server(my_app, "0.0.0.0", parse(Int, ENV["PORT"]))
-#route!(server, "/" => App(DOM.div("hello world")))
+server.proxy_url = url
 
 wait(server)
